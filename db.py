b18c3919d6d3318242cb0cb50d8db0ad534ac63c
He Xiaocong <xiaocong@gmail.com>
2014-03-21 19:23:04 +0800

    updae mongo config

--- db.py
+++ db.py
@@ -9,7 +9,9 @@ redis_host = os.environ.get('REDIS_SERVER', 'localhost')
 redis_port = int(os.environ.get('REDIS_PORT', 6379))
 redis_db = int(os.environ.get('REDIS_DB', 1))
 pool = _redis.ConnectionPool(host=redis_host, port=redis_port, db=redis_db)
-mongo_client = MongoClient()
+
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017/')
+mongo_client = MongoClient(mongo_url)
 
 
 def redis():
