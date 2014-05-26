aa71da503e743e6ab8ccb1e318d8f5d82fac0e84
He Xiaocong <xiaocong@gmail.com>
2014-05-26 11:22:05 +0800

    remove gevent from worker

--- start_beat.sh
+++ start_beat.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-celery worker --app=ghdata.worker:worker --detach -P gevent -c 10 -Q celery --loglevel=INFO --logfile=/tmp/celeryd.assigner.log --pidfile=/tmp/celeryd.assigner.pid
+celery worker --app=ghdata.worker:worker --detach -c 10 -Q celery --loglevel=INFO --logfile=/tmp/celeryd.assigner.log --pidfile=/tmp/celeryd.assigner.pid
 celery beat --app=ghdata.worker:worker --detach -f /tmp/celeryd.beat.log --pidfile=/tmp/celeryd.beat.pid
